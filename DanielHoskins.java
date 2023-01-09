005c3b58ee93d1eaad56bf787289885e4e2cccb6
Daniel Hoskins <daniel.hoskins@mrjaffesclass.com>
2023-01-09 14:21:59 -0800

    Finished Java Zoo. 1/9/23

---
+++ DanielHoskins.java
@@ -0,0 +1,29 @@
+
+/**
+ * A BoopleSnoot is Mr. Jaffe's animal
+ * 
+ * @author Mr. Jaffe
+ * @version 1.0 2017-07-14
+ */
+public class DanielHoskins extends Animal
+{
+  public DanielHoskins() {
+    super("DanielHoskins", "Brown", 4);
+  }
+  
+  public String speak() {
+    return "Bark";
+  }
+  
+  public String getDisposition() {
+    return "Goofy";
+  }
+  
+  public String getType() {
+    return "Dog";
+  }
+  
+  public String getName() {
+    return "Goose";
+  }
+}
